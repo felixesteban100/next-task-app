94f4e13651e4c3c45375e889e6972ddbd7a82b5e
felixesteban100 <felixesteban100@gmail.com>
2025-03-23 22:37:26 -0400

    Add brand new animated titles in the navbar with animated icons/emojis using lottie files

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,6 +12,33 @@ import {
     TooltipProvider,
     TooltipTrigger,
 } from "@/components/ui/tooltip"
+import { useEffect, useState } from "react"
+import { AnimatePresence, motion } from "motion/react"
+import Lottie from 'lottie-react'; // Import lottie-react for better integration
+// import { EMOJIS_CODE } from "@/constants"
+
+import Shine from "@/public/emoji/Shine.json"
+import HandsPray from "@/public/emoji/HandsPray.json"
+// import Brain from "@/public/emoji/Brain.json"
+// import Palms from "@/public/emoji/Palms.json"
+// import Heart from "@/public/emoji/Heart.json"
+import Praise from "@/public/emoji/Praise.json"
+// import CloudThinking from "@/public/emoji/CloudThinking.json"
+import Lightning from "@/public/emoji/Lightning.json"
+// import Think from "@/public/emoji/Think.json"
+import Fire from "@/public/emoji/Fire.json"
+import StarShine from "@/public/emoji/StarShine.json"
+// import Cross from "@/public/emoji/Cross.json"
+import Lightbulb from "@/public/emoji/Lightbulb.json"
+import HeartShine from "@/public/emoji/HeartShine.json"
+// import Dove from "@/public/emoji/Dove.json"
+
+
+import Bible from "@/public/emoji/Bible.json"
+import ThinkingMechanics from "@/public/emoji/ThinkingMechanics.json"
+import RunningMan from "@/public/emoji/RunningMan.json"
+import HeartFilled from "@/public/emoji/HeartFilled.json"
+
 
 const links = [
     {
@@ -31,14 +58,93 @@ const links = [
     },
 ]
 
+const titles = [
+    "Glorify God in All Things", // ✨🙏
+    "Glorify God in Mind, Deed, and Heart", // 🧠🤲❤️
+    "Honor God in Thought, Action, Intention", // 🙌💭⚡
+    "Think, Act, Intend for God's Glory", // 🤔🔥🌟
+    "Glorify God in All You Do", // ✝️💡💖
+    "Every Thought & Action for God", // 🕊️💭👐
+]
+
+const emojis = [
+    [HandsPray, Shine],
+    [ThinkingMechanics, RunningMan, HeartFilled /* Brain, */ /* Palms, *//*  Heart */],
+    [Praise, /* CloudThinking, */ Lightning],
+    [/* Think, */ Fire, StarShine],
+    [Bible,/* Cross,  */Lightbulb, HeartShine],
+    [ThinkingMechanics, RunningMan/* Dove, */ /* CloudThinking, *//* Palms */],
+]
+
+/* const link = [
+    // "https://fonts.gstatic.com/s/e/notoemoji/latest/2728/lottie.json",
+    // "https://fonts.gstatic.com/s/e/notoemoji/latest/1f64f/lottie.json",
+    // "https://fonts.gstatic.com/s/e/notoemoji/latest/undefined/lottie.json",
+    // "https://fonts.gstatic.com/s/e/notoemoji/latest/undefined/lottie.json",
+    // "https://fonts.gstatic.com/s/e/notoemoji/latest/undefined/lottie.json",
+    // "https://fonts.gstatic.com/s/e/notoemoji/latest/1f64c/lottie.json",
+    // "https://fonts.gstatic.com/s/e/notoemoji/latest/undefined/lottie.json", // CloudThinking
+    // "https://fonts.gstatic.com/s/e/notoemoji/latest/26a1/lottie.json",
+    // "https://fonts.gstatic.com/s/e/notoemoji/latest/1f914/lottie.json",
+    // "https://fonts.gstatic.com/s/e/notoemoji/latest/1f525/lottie.json",
+    // "https://fonts.gstatic.com/s/e/notoemoji/latest/1f31f/lottie.json", // StarShine
+    // "https://fonts.gstatic.com/s/e/notoemoji/latest/undefined/lottie.json", // Cross
+    // "https://fonts.gstatic.com/s/e/notoemoji/latest/1f4a1/lottie.json",
+    // "https://fonts.gstatic.com/s/e/notoemoji/latest/1f496/lottie.json",
+    // "https://fonts.gstatic.com/s/e/notoemoji/latest/undefined/lottie.json", // Dove
+] */
+
+// function getLink(emojiCode: string) {
+//     return `https://fonts.gstatic.com/s/e/notoemoji/latest/${emojiCode}/lottie.json`
+// }
+
+// function getCode(emoji: string): string {
+//     return Object.entries(EMOJIS_CODE).filter(([, { value }]) => value === emoji).map(c => c[1].key)[0]
+// }
+
+// console.log(emojis.map(emoji => getLink(getCode(emoji))))
+
+
 export default function Navbar() {
     const pathname = usePathname()
     const { isLoaded, isSignedIn } = useAuth()
 
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % titles.length);
+        }, 5000); // Change text every 2 seconds
+        return () => clearInterval(intervalId); // Cleanup interval on unmount
+    }, []);
+
     return (
         <nav className='h-fit bg-secondary flex flex-row justify-between py-2 px-10 fixed top-0 w-screen z-50'>
-            <Link href={`/`}>
-                <h1 className="text-2xl font-bold text-center mt-2">GLORIFY GOD IN EVERY THOUGHT/ACTION/INTENTION</h1>
+            <Link href={`/`} className="text-2xl font-bold text-center  transition-opacity duration-300 relative w-full">
+                <AnimatePresence>
+                    <motion.h1
+                        key={index}
+                        initial={{ y: 20, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        exit={{ y: -20, opacity: 0 }}
+                        transition={{ ease: "easeInOut" }}
+                        style={{ position: "absolute" }}
+                        className="flex items-center gap-2"
+                    >
+                        {titles[index]}
+                        {emojis[index].map((emoji, i) => {
+                            return (
+                                <Lottie
+                                    key={i}
+                                    animationData={emoji}  // Pass in the JSON animation file
+                                    loop // Loop the animation
+                                    autoplay // Play animation when component mounts
+                                    style={{ width: 35, height: 35 }}
+                                />
+                            )
+                        })}
+                    </motion.h1>
+                </AnimatePresence>
             </Link>
             <div className="flex gap-2 items-center">
                 {!isLoaded && <Loader className="animate-spin" />}
--- constants/index.ts
+++ constants/index.ts
@@ -786,3 +786,1083 @@ export const TIMES = [
     "9:10 pm",
     "10:00 pm"
 ];
+
+
+export const EMOJIS_CODE: { key: string, value: string }[] = [
+    {
+        key: "1f600",
+        value: "😀"
+    },
+    {
+        key: "1f603",
+        value: "😃"
+    },
+    {
+        key: "1f604",
+        value: "😄"
+    },
+    {
+        key: "1f601",
+        value: "😁"
+    },
+    {
+        key: "1f606",
+        value: "😆"
+    },
+    {
+        key: "1f605",
+        value: "😅"
+    },
+    {
+        key: "1f602",
+        value: "😂"
+    },
+    {
+        key: "1f923",
+        value: "🤣"
+    },
+    {
+        key: "1f62d",
+        value: "😭"
+    },
+    {
+        key: "1f609",
+        value: "😉"
+    },
+    {
+        key: "1f617",
+        value: "😗"
+    },
+    {
+        key: "1f619",
+        value: "😙"
+    },
+    {
+        key: "1f61a",
+        value: "😚"
+    },
+    {
+        key: "1f618",
+        value: "😘"
+    },
+    {
+        key: "1f970",
+        value: "🥰"
+    },
+    {
+        key: "1f60d",
+        value: "😍"
+    },
+    {
+        key: "1f929",
+        value: "🤩"
+    },
+    {
+        key: "1f973",
+        value: "🥳"
+    },
+    {
+        key: "1fae0",
+        value: "🫠"
+    },
+    {
+        key: "1f643",
+        value: "🙃"
+    },
+    {
+        key: "1f642",
+        value: "🙂"
+    },
+    {
+        key: "1f972",
+        value: "🥲"
+    },
+    {
+        key: "1f979",
+        value: "🥹"
+    },
+    {
+        key: "1f60a",
+        value: "😊"
+    },
+    {
+        key: "1f60c",
+        value: "😌"
+    },
+    {
+        key: "1f60f",
+        value: "😏"
+    },
+    {
+        key: "1f634",
+        value: "😴"
+    },
+    {
+        key: "1f62a",
+        value: "😪"
+    },
+    {
+        key: "1f924",
+        value: "🤤"
+    },
+    {
+        key: "1f60b",
+        value: "😋"
+    },
+    {
+        key: "1f61b",
+        value: "😛"
+    },
+    {
+        key: "1f61d",
+        value: "😝"
+    },
+    {
+        key: "1f61c",
+        value: "😜"
+    },
+    {
+        key: "1f92a",
+        value: "🤪"
+    },
+    {
+        key: "1f974",
+        value: "🥴"
+    },
+    {
+        key: "1f614",
+        value: "😔"
+    },
+    {
+        key: "1f97a",
+        value: "🥺"
+    },
+    {
+        key: "1f62c",
+        value: "😬"
+    },
+    {
+        key: "1f611",
+        value: "😑"
+    },
+    {
+        key: "1f610",
+        value: "😐"
+    },
+    {
+        key: "1f636",
+        value: "😶"
+    },
+    {
+        key: "1fae5",
+        value: "🫥"
+    },
+    {
+        key: "1f910",
+        value: "🤐"
+    },
+    {
+        key: "1fae1",
+        value: "🫡"
+    },
+    {
+        key: "1f914",
+        value: "🤔"
+    },
+    {
+        key: "1f92b",
+        value: "🤫"
+    },
+    {
+        key: "1fae2",
+        value: "🫢"
+    },
+    {
+        key: "1f92d",
+        value: "🤭"
+    },
+    {
+        key: "1f971",
+        value: "🥱"
+    },
+    {
+        key: "1f917",
+        value: "🤗"
+    },
+    {
+        key: "1fae3",
+        value: "🫣"
+    },
+    {
+        key: "1f631",
+        value: "😱"
+    },
+    {
+        key: "1f928",
+        value: "🤨"
+    },
+    {
+        key: "1f9d0",
+        value: "🧐"
+    },
+    {
+        key: "1f612",
+        value: "😒"
+    },
+    {
+        key: "1f644",
+        value: "🙄"
+    },
+    {
+        key: "1f624",
+        value: "😤"
+    },
+    {
+        key: "1f620",
+        value: "😠"
+    },
+    {
+        key: "1f621",
+        value: "😡"
+    },
+    {
+        key: "1f92c",
+        value: "🤬"
+    },
+    {
+        key: "1f61e",
+        value: "😞"
+    },
+    {
+        key: "1f613",
+        value: "😓"
+    },
+    {
+        key: "1f61f",
+        value: "😟"
+    },
+    {
+        key: "1f625",
+        value: "😥"
+    },
+    {
+        key: "1f622",
+        value: "😢"
+    },
+    {
+        key: "1f641",
+        value: "🙁"
+    },
+    {
+        key: "1fae4",
+        value: "🫤"
+    },
+    {
+        key: "1f615",
+        value: "😕"
+    },
+    {
+        key: "1f630",
+        value: "😰"
+    },
+    {
+        key: "1f628",
+        value: "😨"
+    },
+    {
+        key: "1f627",
+        value: "😧"
+    },
+    {
+        key: "1f626",
+        value: "😦"
+    },
+    {
+        key: "1f62e",
+        value: "😮"
+    },
+    {
+        key: "1f62f",
+        value: "😯"
+    },
+    {
+        key: "1f632",
+        value: "😲"
+    },
+    {
+        key: "1f633",
+        value: "😳"
+    },
+    {
+        key: "1f92f",
+        value: "🤯"
+    },
+    {
+        key: "1f616",
+        value: "😖"
+    },
+    {
+        key: "1f623",
+        value: "😣"
+    },
+    {
+        key: "1f629",
+        value: "😩"
+    },
+    {
+        key: "1f62b",
+        value: "😫"
+    },
+    {
+        key: "1f635",
+        value: "😵"
+    },
+    {
+        key: "1fae8",
+        value: "🫨"
+    },
+    {
+        key: "1f976",
+        value: "🥶"
+    },
+    {
+        key: "1f975",
+        value: "🥵"
+    },
+    {
+        key: "1f922",
+        value: "🤢"
+    },
+    {
+        key: "1f92e",
+        value: "🤮"
+    },
+    {
+        key: "1f927",
+        value: "🤧"
+    },
+    {
+        key: "1f912",
+        value: "🤒"
+    },
+    {
+        key: "1f915",
+        value: "🤕"
+    },
+    {
+        key: "1f637",
+        value: "😷"
+    },
+    {
+        key: "1f925",
+        value: "🤥"
+    },
+    {
+        key: "1f607",
+        value: "😇"
+    },
+    {
+        key: "1f920",
+        value: "🤠"
+    },
+    {
+        key: "1f911",
+        value: "🤑"
+    },
+    {
+        key: "1f913",
+        value: "🤓"
+    },
+    {
+        key: "1f60e",
+        value: "😎"
+    },
+    {
+        key: "1f978",
+        value: "🥸"
+    },
+    {
+        key: "1f921",
+        value: "🤡"
+    },
+    {
+        key: "1f608",
+        value: "😈"
+    },
+    {
+        key: "1f47f",
+        value: "👿"
+    },
+    {
+        key: "1f47b",
+        value: "👻"
+    },
+    {
+        key: "1f383",
+        value: "🎃"
+    },
+    {
+        key: "1f4a9",
+        value: "💩"
+    },
+    {
+        key: "1f916",
+        value: "🤖"
+    },
+    {
+        key: "1f47d",
+        value: "👽"
+    },
+    {
+        key: "1f31b",
+        value: "🌛"
+    },
+    {
+        key: "1f31c",
+        value: "🌜"
+    },
+    {
+        key: "1f31e",
+        value: "🌞"
+    },
+    {
+        key: "1f525",
+        value: "🔥"
+    },
+    {
+        key: "1f4af",
+        value: "💯"
+    },
+    {
+        key: "1f31f",
+        value: "🌟"
+    },
+    {
+        key: "1f4a5",
+        value: "💥"
+    },
+    {
+        key: "1f389",
+        value: "🎉"
+    },
+    {
+        key: "1f648",
+        value: "🙈"
+    },
+    {
+        key: "1f649",
+        value: "🙉"
+    },
+    {
+        key: "1f64a",
+        value: "🙊"
+    },
+    {
+        key: "1f63a",
+        value: "😺"
+    },
+    {
+        key: "1f638",
+        value: "😸"
+    },
+    {
+        key: "1f639",
+        value: "😹"
+    },
+    {
+        key: "1f63b",
+        value: "😻"
+    },
+    {
+        key: "1f63c",
+        value: "😼"
+    },
+    {
+        key: "1f63d",
+        value: "😽"
+    },
+    {
+        key: "1f640",
+        value: "🙀"
+    },
+    {
+        key: "1f63f",
+        value: "😿"
+    },
+    {
+        key: "1f63e",
+        value: "😾"
+    },
+    {
+        key: "1f9e1",
+        value: "🧡"
+    },
+    {
+        key: "1f49b",
+        value: "💛"
+    },
+    {
+        key: "1f49a",
+        value: "💚"
+    },
+    {
+        key: "1fa75",
+        value: "🩵"
+    },
+    {
+        key: "1f499",
+        value: "💙"
+    },
+    {
+        key: "1f49c",
+        value: "💜"
+    },
+    {
+        key: "1f90e",
+        value: "🤎"
+    },
+    {
+        key: "1f5a4",
+        value: "🖤"
+    },
+    {
+        key: "1fa76",
+        value: "🩶"
+    },
+    {
+        key: "1f90d",
+        value: "🤍"
+    },
+    {
+        key: "1fa77",
+        value: "🩷"
+    },
+    {
+        key: "1f498",
+        value: "💘"
+    },
+    {
+        key: "1f49d",
+        value: "💝"
+    },
+    {
+        key: "1f496",
+        value: "💖"
+    },
+    {
+        key: "1f497",
+        value: "💗"
+    },
+    {
+        key: "1f493",
+        value: "💓"
+    },
+    {
+        key: "1f49e",
+        value: "💞"
+    },
+    {
+        key: "1f495",
+        value: "💕"
+    },
+    {
+        key: "1f48c",
+        value: "💌"
+    },
+    {
+        key: "1f494",
+        value: "💔"
+    },
+    {
+        key: "1f48b",
+        value: "💋"
+    },
+    {
+        key: "1f463",
+        value: "👣"
+    },
+    {
+        key: "1fac0",
+        value: "🫀"
+    },
+    {
+        key: "1fa78",
+        value: "🩸"
+    },
+    {
+        key: "1f9a0",
+        value: "🦠"
+    },
+    {
+        key: "1f480",
+        value: "💀"
+    },
+    {
+        key: "1f440",
+        value: "👀"
+    },
+    {
+        key: "1fae6",
+        value: "🫦"
+    },
+    {
+        key: "1f9bf",
+        value: "🦿"
+    },
+    {
+        key: "1f9be",
+        value: "🦾"
+    },
+    {
+        key: "1f4aa",
+        value: "💪"
+    },
+    {
+        key: "1f44f",
+        value: "👏"
+    },
+    {
+        key: "1f44d",
+        value: "👍"
+    },
+    {
+        key: "1f44e",
+        value: "👎"
+    },
+    {
+        key: "1f64c",
+        value: "🙌"
+    },
+    {
+        key: "1f44b",
+        value: "👋"
+    },
+    {
+        key: "1f91e",
+        value: "🤞"
+    },
+    {
+        key: "1f64f",
+        value: "🙏"
+    },
+    {
+        key: "1f483",
+        value: "💃"
+    },
+    {
+        key: "1f339",
+        value: "🌹"
+    },
+    {
+        key: "1f940",
+        value: "🥀"
+    },
+    {
+        key: "1f342",
+        value: "🍂"
+    },
+    {
+        key: "1f331",
+        value: "🌱"
+    },
+    {
+        key: "1f340",
+        value: "🍀"
+    },
+    {
+        key: "1f30b",
+        value: "🌋"
+    },
+    {
+        key: "1f305",
+        value: "🌅"
+    },
+    {
+        key: "1f304",
+        value: "🌄"
+    },
+    {
+        key: "1f308",
+        value: "🌈"
+    },
+    {
+        key: "26a1",
+        value: "⚡"
+    },
+    {
+        key: "1f4ab",
+        value: "💫"
+    },
+    {
+        key: "1f30d",
+        value: "🌍"
+    },
+    {
+        key: "1f984",
+        value: "🦄"
+    },
+    {
+        key: "1f98e",
+        value: "🦎"
+    },
+    {
+        key: "1f409",
+        value: "🐉"
+    },
+    {
+        key: "1f996",
+        value: "🦖"
+    },
+    {
+        key: "1f422",
+        value: "🐢"
+    },
+    {
+        key: "1f40d",
+        value: "🐍"
+    },
+    {
+        key: "1f438",
+        value: "🐸"
+    },
+    {
+        key: "1f407",
+        value: "🐇"
+    },
+    {
+        key: "1f400",
+        value: "🐀"
+    },
+    {
+        key: "1f415",
+        value: "🐕"
+    },
+    {
+        key: "1f416",
+        value: "🐖"
+    },
+    {
+        key: "1f40e",
+        value: "🐎"
+    },
+    {
+        key: "1facf",
+        value: "🫏"
+    },
+    {
+        key: "1f402",
+        value: "🐂"
+    },
+    {
+        key: "1f410",
+        value: "🐐"
+    },
+    {
+        key: "1f998",
+        value: "🦘"
+    },
+    {
+        key: "1f405",
+        value: "🐅"
+    },
+    {
+        key: "1f412",
+        value: "🐒"
+    },
+    {
+        key: "1f9a6",
+        value: "🦦"
+    },
+    {
+        key: "1f987",
+        value: "🦇"
+    },
+    {
+        key: "1f413",
+        value: "🐓"
+    },
+    {
+        key: "1f423",
+        value: "🐣"
+    },
+    {
+        key: "1f424",
+        value: "🐤"
+    },
+    {
+        key: "1f425",
+        value: "🐥"
+    },
+    {
+        key: "1f985",
+        value: "🦅"
+    },
+    {
+        key: "1fabf",
+        value: "🪿"
+    },
+    {
+        key: "1f99a",
+        value: "🦚"
+    },
+    {
+        key: "1f9ad",
+        value: "🦭"
+    },
+    {
+        key: "1f42c",
+        value: "🐬"
+    },
+    {
+        key: "1f433",
+        value: "🐳"
+    },
+    {
+        key: "1f421",
+        value: "🐡"
+    },
+    {
+        key: "1f980",
+        value: "🦀"
+    },
+    {
+        key: "1f419",
+        value: "🐙"
+    },
+    {
+        key: "1fabc",
+        value: "🪼"
+    },
+    {
+        key: "1f40c",
+        value: "🐌"
+    },
+    {
+        key: "1f41c",
+        value: "🐜"
+    },
+    {
+        key: "1f99f",
+        value: "🦟"
+    },
+    {
+        key: "1f41d",
+        value: "🐝"
+    },
+    {
+        key: "1f98b",
+        value: "🦋"
+    },
+    {
+        key: "1f43e",
+        value: "🐾"
+    },
+    {
+        key: "1f345",
+        value: "🍅"
+    },
+    {
+        key: "1f37f",
+        value: "🍿"
+    },
+    {
+        key: "1f37b",
+        value: "🍻"
+    },
+    {
+        key: "1f942",
+        value: "🥂"
+    },
+    {
+        key: "1f37e",
+        value: "🍾"
+    },
+    {
+        key: "1f377",
+        value: "🍷"
+    },
+    {
+        key: "1f379",
+        value: "🍹"
+    },
+    {
+        key: "1f6a8",
+        value: "🚨"
+    },
+    {
+        key: "1f6f8",
+        value: "🛸"
+    },
+    {
+        key: "1f680",
+        value: "🚀"
+    },
+    {
+        key: "1f6eb",
+        value: "🛫"
+    },
+    {
+        key: "1f6ec",
+        value: "🛬"
+    },
+    {
+        key: "1f3a2",
+        value: "🎢"
+    },
+    {
+        key: "1f38a",
+        value: "🎊"
+    },
+    {
+        key: "1f388",
+        value: "🎈"
+    },
+    {
+        key: "1f382",
+        value: "🎂"
+    },
+    {
+        key: "1f386",
+        value: "🎆"
+    },
+    {
+        key: "1faa9",
+        value: "🪩"
+    },
+    {
+        key: "26bd",
+        value: "⚽"
+    },
+    {
+        key: "1f3af",
+        value: "🎯"
+    },
+    {
+        key: "1f3bb",
+        value: "🎻"
+    },
+    {
+        key: "1f941",
+        value: "🥁"
+    },
+    {
+        key: "1fa87",
+        value: "🪇"
+    },
+    {
+        key: "1f50b",
+        value: "🔋"
+    },
+    {
+        key: "1faab",
+        value: "🪫"
+    },
+    {
+        key: "1f4b8",
+        value: "💸"
+    },
+    {
+        key: "1f4a1",
+        value: "💡"
+    },
+    {
+        key: "1f393",
+        value: "🎓"
+    },
+    {
+        key: "1f48e",
+        value: "💎"
+    },
+    {
+        key: "23f0",
+        value: "⏰"
+    },
+    {
+        key: "1f514",
+        value: "🔔"
+    },
+    {
+        key: "274c",
+        value: "❌"
+    },
+    {
+        key: "1f3b6",
+        value: "🎶"
+    },
+    {
+        key: "1f192",
+        value: "🆒"
+    },
+    {
+        key: "1f3c1",
+        value: "🏁"
+    },
+    {
+        key: "2615",
+        value: "☕"
+    },
+    {
+        key: "2705",
+        value: "✅"
+    },
+    {
+        key: "2728",
+        value: "✨"
+    },
+    {
+        key: "2648",
+        value: "♈"
+    },
+    {
+        key: "2649",
+        value: "♉"
+    },
+    {
+        key: "2650",
+        value: "♐"
+    },
+    {
+        key: "2651",
+        value: "♑"
+    },
+    {
+        key: "2652",
+        value: "♒"
+    },
+    {
+        key: "2653",
+        value: "♓"
+    },
+    {
+        key: "2795",
+        value: "➕"
+    },
+    {
+        key: "264a",
+        value: "♊"
+    },
+    {
+        key: "264b",
+        value: "♋"
+    },
+    {
+        key: "264c",
+        value: "♌"
+    },
+    {
+        key: "264d",
+        value: "♍"
+    },
+    {
+        key: "264e",
+        value: "♎"
+    },
+    {
+        key: "264f",
+        value: "♏"
+    },
+    {
+        key: "26ce",
+        value: "⛎"
+    }
+];
