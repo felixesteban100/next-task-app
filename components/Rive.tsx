4fb4ae1c7909e4e52294139ad7e9993476e750da
felixesteban100 <felixesteban100@gmail.com>
2025-03-26 18:37:27 -0400

    Change rive animation in the home page to the vault boy from fallout

--- components/Rive.tsx
+++ components/Rive.tsx
@@ -1,48 +1,34 @@
 "use client"
 
 import { useRive } from '@rive-app/react-canvas';
-// import { Button } from './ui/button';
-// import { useState } from 'react';
 
 export default function Simple() {
-    // const [isPlaying, setIsPlaying] = useState(true);
-    // const [animationText, setAnimationText] = useState('');
+    // const {
+    //     rive,
+    //     RiveComponent: RiveComponentPlayback
+    // } = useRive({
+    //     src: 'truck.riv',
+    //     stateMachines: ["drive", "bounce"],
+    //     artboard: 'Truck',
+    //     autoplay: true,
+    // });
+    // const bumpInput = rive?.stateMachineInputs("drive")[0];
+
     const {
-        rive,
+        // rive,
         RiveComponent: RiveComponentPlayback
     } = useRive({
-        src: 'truck.riv',
-        stateMachines: ["drive", "bounce"],
-        // stateMachines: "Bump",
-        artboard: 'Truck',
+        src: 'vault_boy.riv',
+        stateMachines: ["State Machine 1"],
+        artboard: 'Vault Boy',
         autoplay: true,
-        // onPause: () => {
-        //     setAnimationText('Animation paused!');
-        // },
-        // onPlay: () => {
-        //     setAnimationText('Animation is playing..');
-        // }
     });
-
-    const bumpInput = rive?.stateMachineInputs("drive")[0];
-
-    // const togglePlaying = () => {
-    //     if (rive == null) return
-
-    //     if (isPlaying) {
-    //         rive.pause();
-    //         setIsPlaying(false);
-    //     } else {
-    //         // rive.play();
-    //         setIsPlaying(true);
-    //     }
-    // };
+    // const thumbsUp = rive?.stateMachineInputs("Is pressed")[0]
 
     return <>
         <div className="center" style={{ width: '800px', height: '500px' }}>
-            <RiveComponentPlayback onClick={() => bumpInput && bumpInput.fire()} className="h-full w-auto" />
-            {/* <p>{animationText}</p> */}
-            {/* <Button onClick={togglePlaying}>{isPlaying ? 'Pause' : 'Play'}</Button> */}
+            {/* <RiveComponentPlayback onClick={() => bumpInput && bumpInput.fire()} className="h-full w-auto" /> */}
+            <RiveComponentPlayback /* onClick={() => thumbsUp && thumbsUp.fire()}  */ className="h-full w-auto" />
         </div>
     </>;
 }
