47deefe68c70def4763c7bbf6e9d6c6d3add4f5d
felixesteban100 <felixesteban100@gmail.com>
2025-12-28 12:13:48 -0500

    Set steps of Godly routine

--- app/api/socket/route.ts
+++ app/api/socket/route.ts
@@ -1,45 +1,47 @@
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 // import { WebSocketServer } from "ws";
 // import { dbChangeEmitter } from "@/db/mongodb/mongodb";
 
 // let wss: WebSocketServer | null = null;
 
-// export const GET = async () => {
-//   // Create a response that upgrades to WebSocket
-//   const { response } = NextResponse.next({ webSocket: true });
+export const GET = async () => {
+    //   // Create a response that upgrades to WebSocket
+    //   const { response } = NextResponse.next({ webSocket: true });
 
-//   // @ts-ignore: WebSocket upgrade not typed yet
-//   const ws = response.webSocket;
+    //   // @ts-ignore: WebSocket upgrade not typed yet
+    //   const ws = response.webSocket;
 
-//   if (!wss) {
-//     wss = new WebSocketServer({ noServer: true });
-//   }
+    //   if (!wss) {
+    //     wss = new WebSocketServer({ noServer: true });
+    //   }
 
-//   ws.on("connection", (socket: any) => {
-//     console.log("Client connected");
+    //   ws.on("connection", (socket: any) => {
+    //     console.log("Client connected");
 
-//     // Listener functions for each collection
-//     const listeners: Record<string, (change: any) => void> = {};
+    //     // Listener functions for each collection
+    //     const listeners: Record<string, (change: any) => void> = {};
 
-//     const COLLECTIONS = ["Task", "DEFAULT_TASKS", "Reflection Questions", "List to watch"];
+    //     const COLLECTIONS = ["Task", "DEFAULT_TASKS", "Reflection Questions", "List to watch"];
 
-//     COLLECTIONS.forEach((collectionName) => {
-//       const listener = (change: any) => {
-//         if (socket.readyState === socket.OPEN) {
-//           socket.send(JSON.stringify({ collection: collectionName, change }));
-//         }
-//       };
-//       listeners[collectionName] = listener;
-//       dbChangeEmitter.on(collectionName, listener);
-//     });
+    //     COLLECTIONS.forEach((collectionName) => {
+    //       const listener = (change: any) => {
+    //         if (socket.readyState === socket.OPEN) {
+    //           socket.send(JSON.stringify({ collection: collectionName, change }));
+    //         }
+    //       };
+    //       listeners[collectionName] = listener;
+    //       dbChangeEmitter.on(collectionName, listener);
+    //     });
 
-//     socket.on("close", () => {
-//       console.log("Client disconnected");
-//       COLLECTIONS.forEach((collectionName) => {
-//         dbChangeEmitter.off(collectionName, listeners[collectionName]);
-//       });
-//     });
-//   });
+    //     socket.on("close", () => {
+    //       console.log("Client disconnected");
+    //       COLLECTIONS.forEach((collectionName) => {
+    //         dbChangeEmitter.off(collectionName, listeners[collectionName]);
+    //       });
+    //     });
+    //   });
 
-//   return response;
-// };
+    //   return response;
+
+    return NextResponse.json({ message: "WebSocket endpoint is disabled." });
+};
