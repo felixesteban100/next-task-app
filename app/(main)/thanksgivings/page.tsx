86979f181f9f9fe9fd1e85c2305c1589e4432101
felixesteban100 <felixesteban100@gmail.com>
2026-02-05 08:54:48 -0500

    Fix Thanksgivings card ui

--- app/(main)/thanksgivings/page.tsx
+++ app/(main)/thanksgivings/page.tsx
@@ -17,7 +17,7 @@ export default async function page() {
     const personalThanksgivings = await collectionThanksgivings.findOne()
 
     return (
-        <div className='grid grid-cols-1 gap-6 p-4 md:grid-cols-2 lg:grid-cols-3'>
+        <div className='w-full grid grid-cols-1 gap-6 p-4 md:grid-cols-2 lg:grid-cols-3'>
             {personalThanksgivings?.thanksgivings.map((item, index) => (
                 <CardThanksgiving key={index} thanksgiving={item} />
             ))}
--- components/CardThanksgiving.tsx
+++ components/CardThanksgiving.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Thanksgiving } from "@/app/(main)/thanksgivings/page"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
@@ -16,16 +18,15 @@ export function CardThanksgiving({ thanksgiving }: { thanksgiving: Thanksgiving 
     const { date, description, img, name } = thanksgiving
 
     return (
-        <Card className="relative mx-auto w-full max-w-sm py-6 gap-6  pt-0 ">
-            {/* <div className="absolute inset-0 z-30 aspect-video bg-black/35" /> */}
-            <Image
-                src={img}
-                alt="Event cover"
-                width={400}
-                height={225}
-                /* brightness-60 grayscale dark:brightness-40 */
-                className="relative z-20 aspect-video w-full object-cover "
-            />
+        <Card className="relative w-full py-6 gap-6 pt-0">
+            <div className="relative w-full aspect-video">
+                <Image
+                    src={img}
+                    alt={`Image for ${name}`}
+                    fill
+                    className="object-cover"
+                />
+            </div>
             <CardHeader>
                 <CardAction>
                     <Badge variant="secondary">{typeof date === "string" ? date : DateString(date)}</Badge>
